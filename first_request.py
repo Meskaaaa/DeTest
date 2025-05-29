eec76b00ad0e8a268faf691df1cd0fe301d92332
unknown <Meskaaaaa@yandex.ru>
2025-05-29 23:45:41 +0300

    Добавлена новая функция: Запись в excel файл

--- first_request.py
+++ first_request.py
@@ -3,7 +3,7 @@ from os import write
 import requests
 import re
 import csv
-
+### Создаем URL
 base_url = 'https://api.hh.ru/vacancies'
 params = {
      "text":"Python разработчик",
@@ -11,24 +11,39 @@ params = {
      "per_page": 70,
      "page": 0
 }
-with open('hhpars.csv','w',encoding='utf-8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Название', 'Компания', 'Город', 'Зарплата', 'Требования'])
-response = requests.get(base_url,params = params)
-hh_json = response.json().get('items', [])
-for i in hh_json:
-    requirement = i.get('snippet', {}).get('requirement')
-    clean_requirement = re.sub(r'<\/?highlighttext>', '', requirement) if requirement else "Не указано"
-    print(f"Назввание: {i['name']}")
-    print(f"Компания: {i['employer']['name']}")
-    print(f"Город: {i['area']['name']}")
-    print(f"Требования: {clean_requirement}")
-    if i['salary']:
-        salary = i['salary']
-        print(f"Зарплата: {i['salary']['from']} {i['salary']['currency']}")
-    else:
-        print("Зарплата не указана")
-    if i['salary']:
-        max_salary = i['salary']
-        print(f"Макс Зарплата: {i['salary']['to']}")
-    print("-"*50)
+###Открываем блок для записи в excel
+with open('hhpars.csv','w',encoding='utf-8-sig', newline='') as f:
+    writer = csv.writer(f, delimiter=';') ##Указываем разделитель ; чтобы разбить на стлобцы
+    writer.writerow(['Название', 'Компания', 'Город', 'Зарплата от', 'Зарплата до', 'Валюта', 'Требования']) #указываем столбцы
+
+    response = requests.get(base_url,params = params) #Прокидываем запрос
+    hh_json = response.json().get('items', []) #Переводим в json и запрашиваем через get по ключу items
+    #Создаем цикл для переборки json
+    for i in hh_json:
+        ### Запрашиваем требования
+        requirement = i.get('snippet', {}).get('requirement')
+        ###Чистим требования от не нужных знаков
+        clean_requirement = re.sub(r'<\/?highlighttext>', '', requirement) if requirement else "Не указано"
+        ### Обработка зарплаты, может быть none###
+        salary = i.get('salary')
+        ##Работаем с ценой
+        salary_from = salary.get('from') if salary else "Не указано"
+        salary_to = salary.get('to') if salary else "Не указано"
+        currency = salary.get('currency') if salary else "Не указано"
+        ### Записываем в excel
+        writer.writerow([
+            i['name'],
+            i['employer']['name'],
+            i['area']['name'],
+            salary_from,
+            salary_to,
+            currency,
+            clean_requirement
+        ])
+    ### Выводим в консоль
+        print(f"Назввание: {i['name']}")
+        print(f"Компания: {i['employer']['name']}")
+        print(f"Город: {i['area']['name']}")
+        print(f"Требования: {clean_requirement}")
+
+        print("-"*50)
